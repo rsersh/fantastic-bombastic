b12cac5a90c9ce1e5cbce330447189a74ebae155
Rachel Hunt <rsersh@gmail.com>
2019-11-22 23:40:35 -0800

    refactored code removing match flag

--- listcomp.py
+++ listcomp.py
@@ -33,60 +33,41 @@ def check_equality(list1, list2):
        - return SAME_UNORDERED_DEDUPED if they have the same unordered content
          and reduced to unique items
        - return NO_EQUALITY if none of the previous cases match"""
-    match = 0
-    while True:
-        if id(list1) == id(list2):
-            match = 4
-            break
-        if len(list1) != len(list2):
-            dedupe_unorder_flag = True
-            s1 = set(list1)
-            s2 = set(list2)
-            if len(s1) != len(s2):
-                match = 0
-                break
-            else:
-                for item in s1:
-                    if item not in s2: # try mapping here w any() or set notation or lambda one-line
-                        dedupe_unorder_flag = False
-                if dedupe_unorder_flag == True:
-                    match = 1
-                    break
-                else:
-                    match = 0
-                    break
+    if id(list1) == id(list2):
+        return Equality(4)
+    elif list1 == list2:
+        return Equality(3)
+    elif len(list1) == len(list2):
+        z = [x for x in list1 if x not in list2]
+        if len(z) == 0:
+            return Equality(2)
         else:
-            if list1 == list2:
-                match = 3
-                break 
-            else:
-                same_unorder_flag = True 
-                for item in list1:
-                    if item not in list2:
-                        same_unorder_flag = False
-                if same_unorder_flag == True:
-                    match = 2
-                    break
-                else:
-                    match = 0
-                    break
-    return Equality(match)
+            return Equality(0)
+    elif len(list1) != len(list2):
+        s1 = set(list1)
+        s2 = set(list2)
+        if s1 >= s2:
+            return Equality(1)
+        else:
+            return Equality(0)
+    else:
+        return Equality(0)
 
 
 
 #alist = [1,2,3] 
 #blist = [4,5,6] 
 #print(check_equality(alist, blist))
-alist = [1,2,3,4] 
+#alist = [1,2,3,4] 
 #blist = alist 
 #clist = alist[:] 
 #print(check_equality(alist, blist))
 #print(check_equality(alist, clist))
-elist = [1,2,3,4] 
+#elist = [1,2,3,4] 
 #flist = [4,2,3,1] 
-print(check_equality(alist, elist))
-#a = [1,2,2,3,4] 
-#b = [1,2,2,3,4, 1,3,4,4] 
-#c = [1,2,2,3,4,1, 3, 4, 4, 5] 
-#print(check_equality(a, b))
-#print(check_equality(a, c))
+#print(check_equality(alist, elist))
+a = [1,2,2,3,4] 
+b = [1,2,2,3,4, 1,3,4,4] 
+c = [1,2,2,3,4,1, 3, 4, 4, 5] 
+print(check_equality(a, b))
+print(check_equality(a, c))
